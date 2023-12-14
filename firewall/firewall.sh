4e0395f18bf7fd2469135986e4cfd59b2e91cf73
zara@enti <zara.zhang@enti.cat>
2023-12-14 11:17:52 +0100

    protocolo mejorado usuario

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 #protocolo solo puede ejecutarlo nosotros
-
+MYIP='10.65.0.44'
 
 iptables -F
 
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 
-iptables -A INPUT -p tcp --sport 22 -s 10.65.0.44 ACCEPT
-iptables -A OUTPUT -p tcp --dport 22 -d 10.65.0.44 ACCEPT
+#pasar en mi maquina pues no quiero
+iptables -P FORWARD DROP
 
-iptables -A INPUT -p tcp --sport 3333 -s 10.65.0.44 ACCEPT
-iptables -A OUTPUT -p tcp --dport 333333 -d 10.65.0.44 ACCEPT
+iptables -A INPUT -p tcp --dport 22 -s $MYIP -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -d $MYIP -j ACCEPT
+
+
+
+iptables -A INPUT -p tcp --dport 3333 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 3333 -d 22 -j ACCEPT
+
